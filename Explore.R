11d7e269a824e13608282973d7530266ece1292a
alexa weber <alexaweber@alexas-air.myfiosgateway.com>
2022-04-22 16:53:34 -0400

    Basic visualizations

--- Explore.R
+++ Explore.R
@@ -13,10 +13,7 @@ table <- as_tibble(table) %>% select(Number,Track,Streams)
 
 
 #Try using Selector Gadget
-artists <- url %>% read_html() %>% html_nodes("span") %>% html_text()
-#My RStudio keeps freezing when i run the above code. Doing it step by step, it appears the problem happens after running html_text()
-#Let's try to extract without using html_text()
-artists <- url %>% read_html() %>% html_nodes("span") %>% html_text2() # This seems to work for some reason!
+artists <- url %>% read_html() %>% html_nodes("span") %>% html_text2() 
 artists[2] #This is where the artists start being listed
 artists_list <- artists[2:201] %>% as_tibble() %>% rename(Artist=value) #create a list of the artists for the given chart
 
@@ -30,8 +27,27 @@ table <- mutate(table,Artist = artists_list$Artist)
 table$Track <- str_replace_all(table$Track, "(?<=by).+","") #Get rid of everything preceded by "by"
 table$Track <- str_replace_all(table$Track, "by","") #Get rid of "by"
 table$Track <- trimws(table$Track,"right") #Get rid of trailing white space
+#Convert streams to numbers
+table$Streams <- str_replace_all(table$Streams,"[,+]", "")
+table$Streams <- as.integer(table$Streams)
+
+#Issue: Dealing with tracks that have multiple artists
+#Can we split the Artists column somehow?
 
 
+
+#Exploring what we have so far
+top <- table %>% mutate(Track=reorder(Track,Streams)) %>% head(10)
+ggplot(top) + geom_col(aes(Streams,Track)) + ggtitle("Top Tracks This Week")
+
+#Top artists this week
+topartists <- table %>% group_by(Artist) %>% summarise(n=n()) %>% mutate(Artist=reorder(Artist,n)) %>% filter(n>1)
+ggplot(topartists) + geom_col(aes(n,Artist)) + ggtitle("Top Artists This Week") + xlab("Number of Songs")
+
+#What about artists with only one song on the chart?
+singles <- table %>% group_by(Artist) %>% summarise(n=n()) %>% filter(n==1)
+table %>% semi_join(singles,by="Artist") %>% mutate(Track=reorder(Track,Streams)) %>% head(10) %>%
+  ggplot() + geom_col(aes(Streams,Track)) + ggtitle("Top Tracks By Artists w/ One Song")
 
 
 
